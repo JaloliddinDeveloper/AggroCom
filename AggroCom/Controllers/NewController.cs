75861cb6c109c2e61bb1a9d8fb6c3c2f46d65ae6
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2025-02-08 12:38:42 +0500

    CODE RUB

--- AggroCom/Controllers/NewController.cs
+++ AggroCom/Controllers/NewController.cs
@@ -21,7 +21,7 @@ namespace AggroCom.Controllers
         private readonly IStorageBroker storageBroker;
         private readonly INewsService newsService;
         private readonly string uploadsFolder = "/var/www/files";
-        private readonly string baseUrl = "http://139.59.112.48";
+        private readonly string baseUrl = "http://143.198.217.116";
 
         public NewController(
             IStorageBroker storageBroker, 
