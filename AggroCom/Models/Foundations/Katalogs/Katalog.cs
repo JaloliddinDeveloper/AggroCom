bb602e27a83f86abe1d22db16e05a88f46bb66ec
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2025-03-24 16:01:08 +0500

    Some Fix

--- AggroCom/Models/Foundations/Katalogs/Katalog.cs
+++ AggroCom/Models/Foundations/Katalogs/Katalog.cs
@@ -1,18 +1,22 @@
 ﻿// ---------------------------------------------------------------------------------- 
 // Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
 // ----------------------------------------------------------------------------------
-using System;
-
 namespace AggroCom.Models.Foundations.Katalogs
 {
     public class Katalog
     {
-        public int Id { get; set; }          
-        public string FileName { get; set; } 
+        public int Id { get; set; }
+        public string FileName { get; set; }
         public string FilePicture { get; set; }
-        public string FilePath { get; set; } 
+        public string FilePath { get; set; }
         public long FileSize { get; set; }
         public string NameUz { get; set; }
         public string NameRu { get; set; }
+        public int Type { get; set; }
     }
 }
+
+//ALTER TABLE Katalogs
+//ADD COLUMN NameUz VARCHAR(255) NULL,
+//ADD COLUMN NameRu VARCHAR(255) NULL;
+
